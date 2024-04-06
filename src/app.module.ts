cce9f050cc3d6df590deefc035a603c7c70ab840
Cristian Arias <ariascristian085@gmail.com>
2024-04-05 20:04:58 -0500

    ajustes de errores corregidos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
-import * as dotenv from 'dotenv';
 import { ConfigModule } from '@nestjs/config';
-dotenv.config();
 
 @Module({
   imports: [
